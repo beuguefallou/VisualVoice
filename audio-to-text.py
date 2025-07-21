dccf59f31ebef73caeff053ffb15f93823564abb
fallou <fallou@gmail.com>
2025-07-21 01:13:07 +0000

    update

---
+++ audio-to-text.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import speech_recognition as sr
+from pydub import AudioSegment
+
+
+def convert_audio_to_wav(audio_file):
+    audio = AudioSegment.from_file(audio_file)
+    wav_file = audio_file.name.split(".")[0] + ".wav"
+    audio.export(wav_file, format="wav")
+    return wav_file
+
+
+def speech_to_text(audio_file):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        audio = recognizer.record(source)
+        try:
+            text = recognizer.recognize_google(audio)
+            return text
+        except sr.UnknownValueError:
+            return "Could not understand audio"
+        except sr.RequestError as e:
+            return f"Error: {str(e)}"
+
+
+def main():
+    uploaded_file = "audio.mp3"
+    file_details = {"Filename": "audio", "FileType": "mp3"}
+    print(file_details)
+
+    uploaded_file = convert_audio_to_wav(uploaded_file)
+
+    text = speech_to_text(uploaded_file)
+    print("Converted Text:")
+    print(text)
+
+
+if __name__ == "__main__":
+    main()
---
+++ word2signvid.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import requests
+def save_signed_video(w):
+        url = 'https://www.signingsavvy.com/search/' + w.lower()
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        if soup.find_all('video') == []:
+            new_url = "???"
+            # name/fingerspelling
+            if str(soup).find("(as in ") != -1:
+                new_url = 'https://www.signingsavvy.com/' + [x for x in soup.find_all('a') if str(x).find('sign/') != -1][0].attrs['href']
+            # multiple meanings #TODO: context
+            elif str(soup).find("No direct match on") != -1:
+                new_url = 'https://www.signingsavvy.com/' + f"sign/{w}/0/fingerspell"
+            r = requests.get(new_url)
+            soup = BeautifulSoup(r.content, 'html.parser')
+        new_list = [x for x in soup.find_all('video')[0].contents if str(x).find('src') != -1]
+        video_url = new_list[0].attrs['src']
+        print(f'saving {video_url} to videos/{w}.mp4')
+        #urllib.request.urlretrieve(video_url, f'videos/{w}.mp4')
+        return video_url, url
+
+f = open("subtitles-1.txt", "r")
+text = f.read()
+text = text.replace(";", " ")
+list_of_words = text.lower().split(" ")
+
+for word in list_of_words:
+    save_signed_video(word)
